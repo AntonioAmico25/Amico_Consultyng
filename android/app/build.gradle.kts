374197d096557c9722a7a0b72ad3d6b84397ca0d
AntonioAmico25 <antonio.amico@foca.com.br>
2026-08-24 20:30:42 -0300

    chore(android): atualizar app para homologação 0.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "br.com.amicoconsultyng.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-homolog"
+        versionCode = 2
+        versionName = "0.2.0-homolog"
     }
 
     buildTypes {
